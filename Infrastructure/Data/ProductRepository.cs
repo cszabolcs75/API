36a3dc1900a3b5bd088920aee32124e194f1e27d
T3RXEA <cszabolcs75@gmail.com>
2024-02-21 14:50:01 +0100

    End of section 4

--- Infrastructure/Data/ProductRepository.cs
+++ Infrastructure/Data/ProductRepository.cs
@@ -25,6 +25,8 @@ namespace Infrastructure.Data
 
         public async Task<Product> GetProductByIdAsync(int id)
         {
+            
+
             return await _context.Products
                 .Include(p => p.ProductType)
                 .Include(p => p.ProductBrand)
