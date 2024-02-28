d3b2dfe08fcd43b03eb01302693dd730aa97b15c
T3RXEA <cszabolcs75@gmail.com>
2024-02-28 17:50:26 +0100

    End of section 8.

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,13 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavBarComponent } from "./nav-bar/nav-bar.component";
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { response } from 'express';
+import { CommonModule } from '@angular/common';
+import { Product } from './models/product';
+import { Pagination } from './models/pagination';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [RouterOutlet, NavBarComponent, CommonModule, HttpClientModule]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Skinet';
+  products: Product[] = [];
+  constructor(private http: HttpClient){}
+
+  ngOnInit(): void {
+    this.http.get<Pagination<Product[]>>('https://localhost:5001/api/products?pageSize=50').subscribe({
+      next: response => this.products=response.data,
+      error: error => console.log(error),
+      complete: () =>{
+        console.log('request completed');
+      }
+    })
+  }
 }
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,17 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavBarComponent } from './nav-bar/nav-bar.component'; // Import NavBarComponent
+import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        NavBarComponent // Declare NavBarComponent here
+    ],
+    providers: [HttpClientModule],
+    bootstrap: [AppComponent],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        HttpClientModule,
+        NavBarComponent,
+        CommonModule,
+    ]
 })
 export class AppModule { }
